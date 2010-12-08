23947b77a39d023090f0f717786e80ad25c8aac4
vogon <vogon@minime.serious-net.pb>
2010-12-08 18:09:46 +0100

    jext2_daemon: small fix

--- jext2_daemon.sh
+++ jext2_daemon.sh
@@ -5,7 +5,7 @@ export LIBRARY_PATH="/usr/lib:/usr/local/lib:$HOME/opt/lib"
 export JAVA_COMMAND="java \
 -Ddaemon.pidfile="$JEXT2_PIDFILE" \
 -Djava.library.path="$LIBRARY_PATH" \
--jar $SCRIPTPATH/dist/jext2-plusdepends.jar"
+-jar $SCRIPTPATH/dist/jext2-plusdepends.jar "
 
 
 function launch_daemon() 
@@ -18,7 +18,7 @@ EOF
 }
 
 pid=$(launch_daemon $JAVA_COMMAND -d ${*})
-sleep 2
+sleep 3
 
 if ps -p "${pid}" 2>&1 > /dev/null; then
     echo ${pid} > $JEXT2_PIDFILE
