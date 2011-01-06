3b44944ff83864b29ee63175774c27c60d1d2400
Marcel Lauhoff <ml@irq0.org>
2011-01-06 21:25:44 +0100

    added put functions

--- src/jext2/Ext2fsDataTypes.java
+++ src/jext2/Ext2fsDataTypes.java
@@ -71,5 +71,48 @@ public class Ext2fsDataTypes {
 		long leastSig = buffer.getLong(offset + 8);
 		return new UUID(mostSig, leastSig);
 	}
+	
+	public static void putUUID(ByteBuffer buffer, UUID value, int offset) {
+		buffer.order(ByteOrder.BIG_ENDIAN);
+		long mostSig = value.getMostSignificantBits();  buffer.getLong(offset);
+		long leastSig = value.getLeastSignificantBits(); buffer.getLong(offset + 8);
+		
+		buffer.putLong(offset, mostSig);
+		buffer.putLong(offset + 8, leastSig);
+	}
+
+	public static void putDate(ByteBuffer buffer, Date value, int offset) {
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		int unixTime = (int) (value.getTime() / 1000);
+		buffer.putInt(offset, unixTime);
+	}
+	
+	public static void putString(ByteBuffer buffer, String value, int len, int offset) {
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		buffer.position(offset);
+		for (int i=0; i<len; i++) {
+			buffer.put((byte)value.charAt(i));
+		}	
+	}
+	
+	public static void putLE64(ByteBuffer buffer, long value, int offset) {
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		buffer.putLong(offset, value);
+	}
+	
+	public static void putLE32(ByteBuffer buffer, int value, int offset) {
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		buffer.putInt(offset, value);
+	}
+	
+	public static void putLE16(ByteBuffer buffer, short value, int offset) {
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		buffer.putShort(offset, value);
+	}
+	
+	public static void putLE8(ByteBuffer buffer, byte value, int offset) {
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		buffer.put(offset, value);
+	}
 
 }
