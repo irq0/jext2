14b10b9adedb5d93aeff152048afbdbf69d706e5
Marcel Lauhoff <ml@irq0.org>
2011-01-16 02:10:57 +0100

    helper script for java remote debugging

---
+++ jext2_remote_debug.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export LIBRARY_PATH="/usr/lib:/usr/local/lib:$HOME/opt/lib"
+java -Xdebug \
+    -Xnoagent \
+    -Djava.compiler=NONE \
+    -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=0.0.0.0:5005 \
+    -Djava.library.path="$LIBRARY_PATH" -jar dist/jext2-plusdepends.jar ${*}
