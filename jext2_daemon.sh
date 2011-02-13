bd2a3a330a7fd421d64ef48c6fdba067e7d66861
Marcel Lauhoff <ml@irq0.org>
2011-02-13 19:30:03 +0100

    added commandline support for -server, -client

--- jext2_daemon.sh
+++ jext2_daemon.sh
@@ -4,8 +4,18 @@ export JEXT2_PIDFILE="$(tempfile)"
 export LIBRARY_PATH="/usr/lib:/usr/local/lib:$HOME/opt/lib"
 export JAVA_COMMAND="java \
 -Ddaemon.pidfile="$JEXT2_PIDFILE" \
--Djava.library.path="$LIBRARY_PATH" \
--jar $SCRIPTPATH/dist/jext2-plusdepends.jar "
+-Djava.library.path="$LIBRARY_PATH""
+
+export COMMANDLINE=${*}
+if [ "$1" = "-server" ]; then
+    JAVA_COMMAND="$JAVA_COMMAND -server"
+    COMMANDLINE=${*:2}
+elif [ "$1" = "-client" ]; then
+    JAVA_COMMAND="$JAVA_COMMAND -client"
+    COMMANDLINE=${*:2}
+fi
+
+JAVA_COMMAND="$JAVA_COMMAND -jar $SCRIPTPATH/dist/jext2-plusdepends.jar"
 
 
 function launch_daemon() 
@@ -17,7 +27,7 @@ echo \${pid}
 EOF
 }
 
-pid=$(launch_daemon $JAVA_COMMAND -d ${*})
+pid=$(launch_daemon $JAVA_COMMAND -d $COMMANDLINE)
 sleep 3
 
 if ps -p "${pid}" 2>&1 > /dev/null; then
@@ -26,3 +36,4 @@ else
     $SCRIPTPATH/jext2.sh -h
     echo "Starting jext2 in daemon mode failed!"
 fi
+
