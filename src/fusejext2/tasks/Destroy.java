98a01bcc3c59fa57e48c54d7c1f980ccab1074e6
Marcel Lauhoff <ml@irq0.org>
2011-10-22 19:53:03 +0200

    ported destroy

---
+++ src/fusejext2/tasks/Destroy.java
@@ -0,0 +1,20 @@
+package fusejext2.tasks;
+
+import java.util.Date;
+
+import jext2.exceptions.IoError;
+import fusejext2.Jext2Context;
+
+public class Destroy extends jlowfuse.async.tasks.Destroy<Jext2Context> {
+	public void run() {
+        try {
+            context.superblock.setLastWrite(new Date());
+            context.superblock.sync();
+            context.blockGroups.syncDescriptors();
+            
+            context.blocks.sync();
+        } catch (IoError e) {
+            System.out.println("IoError on final superblock/block group descr sync");
+        }
+	}
+}
