321cf2c4ac7d4ea98dd611d324a1b0219f59f6b6
Marcel Lauhoff <ml@irq0.org>
2011-10-22 20:54:01 +0200

    ported access

---
+++ src/fusejext2/tasks/Access.java
@@ -0,0 +1,17 @@
+package fusejext2.tasks;
+
+import jlowfuse.FuseReq;
+import jlowfuse.Reply;
+import fusejext2.Jext2Context;
+
+public class Access extends jlowfuse.async.tasks.Access<Jext2Context> {
+
+	public Access(FuseReq arg0, long arg1, int arg2) {
+		super(arg0, arg1, arg2);
+	}
+
+	public void run() {
+	    /* Allow everything. Want permissions? Use -o default_permissions */
+        Reply.err(req, 0);
+	}
+}
