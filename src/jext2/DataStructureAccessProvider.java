cbea6352771fa2cb15cf236b2b0bcad1c4f9d675
Marcel Lauhoff <ml@irq0.org>
2011-11-04 16:58:39 +0100

    Added more functionality, mostly for directoryentryaccess

--- src/jext2/DataStructureAccessProvider.java
+++ src/jext2/DataStructureAccessProvider.java
@@ -7,10 +7,10 @@ import java.util.concurrent.locks.ReentrantLock;
 import jext2.exceptions.JExt2Exception;
 
 public abstract class DataStructureAccessProvider<KEY,VAL> {
-	private Map<KEY, ValueAndUsage> table;
-	private ReentrantLock lock;
+	protected Map<KEY, ValueAndUsage> table;
+	protected ReentrantLock lock;
 		
-	private class ValueAndUsage {
+	protected class ValueAndUsage {
 		VAL value;
 		long usage = 0;
 	}
@@ -22,24 +22,47 @@ public abstract class DataStructureAccessProvider<KEY,VAL> {
 	
 	protected abstract VAL createInstance(KEY key) throws JExt2Exception;
 	
+	
+	protected long usageCounter(KEY key) {
+		ValueAndUsage ds = getValueAndUsage(key);
+		
+		if (ds == null || ds.usage <= 0) {
+			return -1;
+		} else {
+			return ds.usage;
+		}
+	}
+	
+	/**
+	 * Add new entry. Usage counter is zero
+	 */
+	protected void add(KEY key, VAL value) {
+		assert value != null;
+	
+		ValueAndUsage ds = new ValueAndUsage();
+		ds.value = value;	
+		
+		lock.lock();
+		table.put(key, ds);
+		lock.unlock();
+	}
+	
+	
 	/**
 	 * Open the entry. Increases usage counter and creates an instance if necessary
 	 */
 	protected VAL open(KEY key) throws JExt2Exception {
-		lock.lock();
-		
+		lock.lock();		
 		ValueAndUsage ds = table.get(key);
+		lock.unlock();	
 		
 		if (ds == null) {
-			ds = new ValueAndUsage();
-			ds.value = createInstance(key);
-			table.put(key, ds);
+			add(key, createInstance(key));
 		}
 		
 		assert ds != null;	
 		ds.usage += 1;
 		
-		lock.unlock();	
 		return ds.value;
 	}
 	
@@ -61,20 +84,25 @@ public abstract class DataStructureAccessProvider<KEY,VAL> {
 			return ds.value;
 		}
 	}
+	
+	private ValueAndUsage getValueAndUsage(KEY key) {
+		lock.lock();
+		ValueAndUsage ds = table.get(key);
+		lock.unlock();
+		
+		return ds;
+	}
 
 	/**
 	 * Retrieve entry already in table. Does not create a new one or change 
 	 * the usage counter 
 	 */
 	protected VAL get(KEY key) {
-		lock.lock();
-		ValueAndUsage ds = table.get(key);
-
+		ValueAndUsage ds = getValueAndUsage(key);
+		
 		if (ds == null || ds.usage <= 0) {
-			lock.unlock();	
 			return null;
 		} else {
-			lock.unlock();
 			assert ds.value != null;
 			return ds.value;
 		}
