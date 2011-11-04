86ce68a1b4e23c9c54b8a87445be102e54602466
Marcel Lauhoff <ml@irq0.org>
2011-11-04 18:38:18 +0100

    Thread safe code for symlink
     * and some cleanup

--- src/jext2/SymlinkInode.java
+++ src/jext2/SymlinkInode.java
@@ -2,6 +2,7 @@ package jext2;
 
 import java.nio.ByteBuffer;
 import java.util.Date;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
@@ -14,28 +15,80 @@ public class SymlinkInode extends DataInode {
 	private String symlink = "";
 
 	public int FAST_SYMLINK_MAX=Constants.EXT2_N_BLOCKS * 4;
+	
+	private ReentrantReadWriteLock symlinkLock = new ReentrantReadWriteLock(true);
 
-	  
+	@NotThreadSafe(useLock=true)
     private String readSlowSymlink() throws JExt2Exception, FileTooLarge {
         ByteBuffer buf = readData((int)getSize(), 0);
         return Ext2fsDataTypes.getString(buf, 0, buf.limit());
-    }       
-    private void writeSlowSymlink(String link) throws JExt2Exception, NoSpaceLeftOnDevice, FileTooLarge {
+    }
+	
+	@NotThreadSafe(useLock=true)
+    private void writeSlowSymlink(String link, int size) throws JExt2Exception, NoSpaceLeftOnDevice, FileTooLarge {
         ByteBuffer buf = ByteBuffer.allocate(Ext2fsDataTypes.getStringByteLength(link));
         Ext2fsDataTypes.putString(buf, link, buf.capacity(), 0);
         buf.rewind();
         writeData(buf, 0);
     }
-    
+
+	@NotThreadSafe(useLock=true)
     private String readFastSymlink(ByteBuffer buf) throws IoError {
         return Ext2fsDataTypes.getString(buf, 40 + offset, (int)getSize());
     }
     
+	@NotThreadSafe(useLock=true)
+    private void writeFastSymlink(String link, int size) throws IoError {
+        this.symlink = link;
+        setSize(size);
+        setBlocks(0);
+        write();
+    }
+
+	@NotThreadSafe(useLock=true)
+    private void writeSymlink(String link, int size) throws NoSpaceLeftOnDevice, FileTooLarge, JExt2Exception {
+
+	    if (size < FAST_SYMLINK_MAX) {
+	    	writeFastSymlink(link, size);
+	    } else {
+	        writeSlowSymlink(link, size);
+	    }
+    }
+
+	@NotThreadSafe(useLock=true)
+    private void cleanupSlowToFast() throws JExt2Exception {
+        try {
+            accessData().truncate(0);
+        } catch (FileTooLarge e) {
+            throw new RuntimeException("should not happen");
+        }
+    }
+
+    private void cleanupFastToSlow() {
+        this.symlink = "";
+    }
+
+	@NotThreadSafe(useLock=true)
+    private void cleanup(String newLink, int newSize) throws JExt2Exception {
+	    if (isSlowSymlink() && newSize <= FAST_SYMLINK_MAX) {
+	    	cleanupFastToSlow();
+	    } else {
+	    	cleanupSlowToFast();
+	    }
+    }
+    
 	public final String getSymlink() throws JExt2Exception, FileTooLarge {
+	    symlinkLock.readLock().lock();
+
+	    String result;    
+	   
 	    if (isFastSymlink())
-	        return symlink;
+	    	result = this.symlink;
 	    else
-	        return readSlowSymlink();
+	    	result = readSlowSymlink();
+	    
+	    symlinkLock.readLock().unlock();
+	    return result;
 	}
 	
 	/**
@@ -44,30 +97,15 @@ public class SymlinkInode extends DataInode {
 	 * @throws FileTooLarge This is unlikely to happen because is requires 
 	 *     a symlink spanning thousands of blocks. 
 	 */
-	public void setSymlink(String link) throws NoSpaceLeftOnDevice, JExt2Exception, FileTooLarge {
-	    int newSize = Ext2fsDataTypes.getStringByteLength(link);
-	    
-	    setSize(0);
+	public void setSymlink(String newLink) throws NoSpaceLeftOnDevice, JExt2Exception, FileTooLarge {
+	    int newSize = Ext2fsDataTypes.getStringByteLength(newLink);  // this is quite expensive and thus passed on
 
-	    /* slow to fast (or vice versa) cleanup */ 
-	    if (!isFastSymlink() && newSize >= FAST_SYMLINK_MAX) {
-	        try {
-	            accessData().truncate(0);
-	        } catch (FileTooLarge e) {
-	            throw new RuntimeException("should not happen");
-	        }
-	    } else {
-	        this.symlink = "";
-	    }
-	            
-	    if (newSize < FAST_SYMLINK_MAX) { /* fast symlink */
-	        this.symlink = link;
-	        setSize(newSize);
-	        setBlocks(0);
-	        write();
-	    } else { /* slow symlink */
-	        writeSlowSymlink(link);
-	    }
+	    setSize(0);
+	    
+	    symlinkLock.writeLock().lock();
+	    cleanup(newLink, newSize);
+	    writeSymlink(newLink, newSize);
+	    symlinkLock.writeLock().unlock();
 
 	    setStatusChangeTime(new Date());
 	}
@@ -84,6 +122,10 @@ public class SymlinkInode extends DataInode {
 	 */
 	public boolean isFastSymlink() {
 	    return (getBlocks() == 0);
+	}
+	
+	public boolean isSlowSymlink() {
+		return !isFastSymlink();
 	}
     
 	protected void read(ByteBuffer buf) throws IoError {
