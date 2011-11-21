290c02377384d30665ab5c949e4fe9e51938d724
Marcel Lauhoff <ml@irq0.org>
2011-11-21 19:49:31 +0100

    Rename Task: Locking

--- src/fusejext2/tasks/Rename.java
+++ src/fusejext2/tasks/Rename.java
@@ -1,7 +1,10 @@
 package fusejext2.tasks;
 
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import jext2.Constants;
 import jext2.DirectoryEntry;
+import jext2.DirectoryEntryAccess;
 import jext2.DirectoryInode;
 import jext2.Inode;
 import jext2.exceptions.JExt2Exception;
@@ -15,16 +18,40 @@ public class Rename extends jlowfuse.async.tasks.Rename<Jext2Context> {
 	public Rename(FuseReq req, long parent, String name, long newparent, String newname) {
 		super(req, parent, name, newparent, newname);
 	}
+	
+	/* XXX this is probably broken */
 
 	@Override
 	public void run() {
 		if (parent == 1) parent = Constants.EXT2_ROOT_INO;
 		if (newparent == 1) newparent = Constants.EXT2_ROOT_INO;
 
+		ReentrantReadWriteLock parentLock = null;
+		ReentrantReadWriteLock newparentLock = null;
+		
 		try {
-			DirectoryInode parentInode = (DirectoryInode)context.inodes.openInode(parent);
-			DirectoryInode newparentInode = (DirectoryInode)context.inodes.openInode(newparent);
-
+			DirectoryInode parentInode;
+			DirectoryInode newparentInode; 
+			try {
+				parentInode = (DirectoryInode)context.inodes.openInode(parent);
+				newparentInode = (DirectoryInode)context.inodes.openInode(newparent);
+			} catch (JExt2Exception e) {
+				throw new RuntimeException("If this ever happens rethink rename command");
+			}
+			
+			parentLock = parentInode.directoryLock();
+			newparentLock = newparentInode.directoryLock();
+			
+			if (parentInode.equals(newparentInode)) {
+				parentLock.writeLock().lock();
+			} else {
+				parentLock.writeLock().lock();
+				newparentLock.writeLock().lock();
+			}
+			
+			DirectoryEntryAccess parentEntries = ((DirectoryInode)parentInode).directoryEntries;
+			DirectoryEntryAccess newparentEntries = ((DirectoryInode)newparentInode).directoryEntries;		
+			
 			/* create entries */
 			DirectoryEntry entry = parentInode.lookup(name);
 
@@ -32,21 +59,26 @@ public class Rename extends jlowfuse.async.tasks.Rename<Jext2Context> {
 			newEntry.setIno(entry.getIno());
 			newEntry.setFileType(entry.getFileType());
 
+			parentEntries.release(entry);
+
 			/*
 			 * When NEW directory entry already exists try to
 			 * delete it.
 			 */
 			try {
 				DirectoryEntry existingEntry = newparentInode.lookup(newname);
-				if (existingEntry.getFileType() == DirectoryEntry.FILETYPE_DIR) {
+				if (existingEntry.isDirectory()) {
 					DirectoryInode existingDir =
 							(DirectoryInode)(context.inodes.openInode(existingEntry.getIno()));
 
 					newparentInode.unLinkDir(existingDir, newname);
+					context.inodes.closeInode(existingDir.getIno());
 				} else {
 					Inode existing = context.inodes.openInode(existingEntry.getIno());
 					newparentInode.unLinkOther(existing, newname);
+					context.inodes.closeInode(existing.getIno());
 				}
+				newparentEntries.release(existingEntry);
 			} catch (NoSuchFileOrDirectory ignored) {
 			}
 
@@ -54,7 +86,7 @@ public class Rename extends jlowfuse.async.tasks.Rename<Jext2Context> {
 			 * When we move a directory we need to update the dot-dot entry
 			 * and the nlinks of the parents.
 			 */
-			if (newEntry.getFileType() == DirectoryEntry.FILETYPE_DIR) {
+			if (newEntry.isDirectory()) {
 				DirectoryInode newDir =
 						(DirectoryInode)(context.inodes.openInode(newEntry.getIno()));
 
@@ -64,6 +96,9 @@ public class Rename extends jlowfuse.async.tasks.Rename<Jext2Context> {
 
 				newparentInode.setLinksCount(newparentInode.getLinksCount() + 1);
 				parentInode.setLinksCount(parentInode.getLinksCount() - 1);
+				
+				newDir.directoryEntries.release(dotdot);
+				context.inodes.closeInode(newDir.getIno());
 			}
 
 			/*
@@ -72,8 +107,27 @@ public class Rename extends jlowfuse.async.tasks.Rename<Jext2Context> {
 			newparentInode.addDirectoryEntry(newEntry);
 			parentInode.removeDirectoryEntry(name);
 
+			assert parentLock.getWriteHoldCount() == 1;
+			assert newparentLock.getWriteHoldCount() == 1;
+
+			if (parentInode.equals(newparentInode)) {
+				parentLock.writeLock().unlock();
+			} else {
+				parentLock.writeLock().unlock();
+				newparentLock.writeLock().unlock();
+			}
+				
 			Reply.err(req, 0);
 		} catch (JExt2Exception e) {
+			assert parentLock.getWriteHoldCount() == 1;
+			assert newparentLock.getWriteHoldCount() == 1;
+
+			if (parentLock.equals(newparentLock)) {
+				parentLock.writeLock().unlock();
+			} else {
+				parentLock.writeLock().unlock();
+				newparentLock.writeLock().unlock();
+			}
 			Reply.err(req, e.getErrno());
 		}
 	}
