0b32847861bcb4f80358339cf83089e22def6523
Marcel Lauhoff <ml@irq0.org>
2012-01-11 17:57:50 +0100

    bugfix: gettattr ist not necessarily  preceeded by open

--- src/fusejext2/tasks/Getattr.java
+++ src/fusejext2/tasks/Getattr.java
@@ -2,6 +2,7 @@ package fusejext2.tasks;
 
 import jext2.Constants;
 import jext2.Inode;
+import jext2.exceptions.JExt2Exception;
 import jlowfuse.FuseReq;
 import jlowfuse.Reply;
 import fuse.FileInfo;
@@ -20,9 +21,15 @@ public class Getattr extends jlowfuse.async.tasks.Getattr<Jext2Context> {
 	@Override
 	public void run() {
 		if (ino == 1) ino = Constants.EXT2_ROOT_INO;
-		Inode inode = context.inodes.getOpened(ino);
-		Stat stat = Util.inodeToStat(context.superblock, inode);
+		Inode inode;
+		try {
+			inode = context.inodes.openInode(ino);
+	 		Stat stat = Util.inodeToStat(context.superblock, inode);
+			Reply.attr(req, stat, 0.0);
+			context.inodes.forgetInode(ino, 1);
+		} catch (JExt2Exception e) {
+			Reply.err(req, e.getErrno());
+		}
 
-		Reply.attr(req, stat, 0.0);
 	}
 }
