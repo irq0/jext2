f9501350abe826047a0d1251b41478b2a208c371
jlow <jlow@jlowtest.(none)>
2012-01-17 16:12:50 +0100

    Use java -server as default for jext2.sh

--- jext2.sh
+++ jext2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export LIBRARY_PATH="/usr/lib:/usr/local/lib:$HOME/opt/lib"
-java -Djava.library.path="$LIBRARY_PATH" -jar dist/jext2-plusdepends.jar ${*}
+java -server -Djava.library.path="$LIBRARY_PATH" -jar dist/jext2-plusdepends.jar ${*}
